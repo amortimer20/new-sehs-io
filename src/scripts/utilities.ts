2e47a838d1e5e2f1af4f27a5ba2d956c33ea2c82
Anthony Mortimer <agm20@outlook.com>
2025-03-29 22:19:03 -0400

    Add color picker interactives

--- src/scripts/utilities.ts
+++ src/scripts/utilities.ts
@@ -1,3 +1,7 @@
+export function mapRange(value: number, inMin: number, inMax: number, outMin: number, outMax: number): number {
+  return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
+
 export function shuffle(array: any[]): void {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
