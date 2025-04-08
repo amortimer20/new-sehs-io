61301d980e87a0e9656da888d9ca4b86b0eb0bfd
Anthony Mortimer <agm20@outlook.com>
2025-04-08 08:03:36 -0400

    major ui update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import alpinejs from '@astrojs/alpinejs';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs()]
+  integrations: [alpinejs(), svelte()]
 });
---
+++ src/components/interactives/BitToggler.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+  let toggles = $state(Array<boolean>(8).fill(false));
+  let bits = $derived(toggles.map<number>((t) => (t ? 1 : 0)));
+  let decValue = $derived(
+    bits.reduce((sum, bit, index) => sum + bit * 2 ** index, 0),
+  );
+</script>
+
+<section>
+  <table class="table">
+    <tbody>
+      <tr class="text-center">
+        <td>
+          <div>{bits[7]}</div>
+          <input
+            class="form-check-input"
+            type="checkbox"
+            bind:checked={toggles[7]}
+          />
+        </td>
+        <td>
+          <div>{bits[6]}</div>
+          <input
+            class="form-check-input"
+            type="checkbox"
+            bind:checked={toggles[6]}
+          />
+        </td>
+        <td>
+          <div>{bits[5]}</div>
+          <input
+            class="form-check-input"
+            type="checkbox"
+            bind:checked={toggles[5]}
+          />
+        </td>
+        <td>
+          <div>{bits[4]}</div>
+          <input
+            class="form-check-input"
+            type="checkbox"
+            bind:checked={toggles[4]}
+          />
+        </td>
+        <td colspan="2"></td>
+        <td>
+          <div>{bits[3]}</div>
+          <input
+            class="form-check-input"
+            type="checkbox"
+            bind:checked={toggles[3]}
+          />
+        </td>
+        <td>
+          <div>{bits[2]}</div>
+          <input
+            class="form-check-input"
+            type="checkbox"
+            bind:checked={toggles[2]}
+          />
+        </td>
+        <td>
+          <div>{bits[1]}</div>
+          <input
+            class="form-check-input"
+            type="checkbox"
+            bind:checked={toggles[1]}
+          />
+        </td>
+        <td>
+          <div>{bits[0]}</div>
+          <input
+            class="form-check-input"
+            type="checkbox"
+            bind:checked={toggles[0]}
+          />
+        </td>
+        <td class="align-top" style:min-width="6.25rem">
+          = {decValue}<sub>DEC</sub>
+        </td>
+      </tr>
+      <tr class="text-center">
+        <td>2<sup>7</sup></td>
+        <td>2<sup>6</sup></td>
+        <td>2<sup>5</sup></td>
+        <td>2<sup>4</sup></td>
+        <td colspan="2"></td>
+        <td>2<sup>3</sup></td>
+        <td>2<sup>2</sup></td>
+        <td>2<sup>1</sup></td>
+        <td>2<sup>0</sup></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+</section>
